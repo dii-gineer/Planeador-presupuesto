f187f364c9e0b1b162395bf8b9bb858d598c3f05
Diana <diana.mendoza.rmz@gmail.com>
2018-11-18 22:08:56 -0600

    Updates

---
+++ precache-manifest.677f658ea4d09840f680b5cdb9fb3743.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0936a1c019b58551f33ec612ec813158",
+    "url": "/dii-gineer/gasto-semanal/static/media/budget.0936a1c0.jpg"
+  },
+  {
+    "revision": "d23caf22794d98444512",
+    "url": "/dii-gineer/gasto-semanal/static/js/runtime~main.d23caf22.js"
+  },
+  {
+    "revision": "1a01d8581d07938bbea8",
+    "url": "/dii-gineer/gasto-semanal/static/js/main.1a01d858.chunk.js"
+  },
+  {
+    "revision": "168c889c3439c029f6e1",
+    "url": "/dii-gineer/gasto-semanal/static/js/1.168c889c.chunk.js"
+  },
+  {
+    "revision": "1a01d8581d07938bbea8",
+    "url": "/dii-gineer/gasto-semanal/static/css/main.c06cb464.chunk.css"
+  },
+  {
+    "revision": "aba989c9eb9f5fd437cc0bd196467772",
+    "url": "/dii-gineer/gasto-semanal/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/dii-gineer/gasto-semanal/precache-manifest.677f658ea4d09840f680b5cdb9fb3743.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/dii-gineer/gasto-semanal/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
